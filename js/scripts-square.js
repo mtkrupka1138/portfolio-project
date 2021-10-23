32ba590ba2076da22edf49af4a1172cb748e6baa
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-10-22 23:00:11 -0400

    daily square and js

---
+++ js/scripts-square.js
@@ -0,0 +1,19 @@
+let tempSquareInputs = sessionStorage.getItem("squareInputs");
+let squareInputs = JSON.parse(tempSquareInputs);
+
+let squareArray = document.getElementsByClassName("square-item");
+
+// set variables for portions of outer square grid, to use later to map inputs to
+// let square1 = document.querySelector("#square1");
+// let square2 = document.querySelector("#square2");
+// let square3 = document.querySelector("#square3");
+// let square4 = document.querySelector("#square4");
+// let square5 = document.querySelector("#square5");
+// let square6 = document.querySelector("#square6");
+// let square7 = document.querySelector("#square7");
+// let square8 = document.querySelector("#square8");
+// let square9 = document.querySelector("#square9");
+// let square10 = document.querySelector("#square10");
+// let square11 = document.querySelector("#square11");
+// let square12 = document.querySelector("#square12");
+// let mainsquare = document.querySelector(".main-square")
--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,18 @@
-// for profile creation
-let profileFormData = []; // declare array in which to store form data
-let value1 = document.getElementById("y-1").value;
-profileFormData.push(value1)
+let squareInputs = document.getElementsByClassName("form-check-input");
+
+sessionStorage.setItem("squareInputs", JSON.stringify(squareInputs));
 
 
-// set variables for portions of outer square grid, to use later to map inputs to
-// let square1 = document.querySelector("#square1");
-// let square2 = document.querySelector("#square2");
-// let square3 = document.querySelector("#square3");
-// let square4 = document.querySelector("#square4");
-// let square5 = document.querySelector("#square5");
-// let square6 = document.querySelector("#square6");
-// let square7 = document.querySelector("#square7");
-// let square8 = document.querySelector("#square8");
-// let square9 = document.querySelector("#square9");
-// let square10 = document.querySelector("#square10");
-// let square11 = document.querySelector("#square11");
-// let square12 = document.querySelector("#square12");
-// let mainsquare = document.querySelector(".main-square")
+let squareInputsChecked = [];
+for (i=0; i< squareInputs.length; i++) {
+    if (squareInputs[i].type="radio") { 
+        if (squareInputs[i].checked) 
+        store.push(squareInputs[i])
+    }
+}
 
 
-// change events for radio buttons on profile creation page
-// let result = document.querySelector('#square1');
-//         document.body.addEventListener('change', function (e) {
-//             let target = e.target;
-//             let message;
-//             switch (target.id) {
-//                 case 'y-1':
-//                     message = document.querySelector('#emoji-1');
-//                     break;
-//                 case 'n-1':
-//                     message = '--';
-//                     break;
-//             }
-//             result.textContent = message;
-//         });
 
-// ele = document.getElementsByClassName("form-check-input");
-// let store 
-// for (i=0; i< ele.length; i++) { if (ele[i].type="radio") { if (ele[i].checked) store.push(ele[i])}}
+
+
+
