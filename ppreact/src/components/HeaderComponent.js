c906215a4d5fda962a797a0cf8a3324531c2f798
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-11-25 19:02:53 -0500

    form redux for login

--- ppreact/src/components/HeaderComponent.js
+++ ppreact/src/components/HeaderComponent.js
@@ -1,3 +1,13 @@
-import React, { Component } from 'react';
+// this is a presentational component to render the header
+
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Daily Square</h1>
+        </header>
+    );
+}
 
 export default Header;
