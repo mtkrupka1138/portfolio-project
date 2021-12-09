64d7e9d8890c1633d498ce397f5fa69d4c08bfb4
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-12-08 20:15:39 -0500

    main and profile creation components

--- ppreact/src/components/CustomSquareFormComponent.js
+++ ppreact/src/components/CustomSquareFormComponent.js
@@ -5,6 +5,44 @@ class CustomSquareForm extends Component {
     render() {
         return (
             <LocalForm>
+                <div class="form-title">
+            <div class="section-title">
+                <p class="sub-section-title">customize your square</p>
+                <p class="mini-p">create parts of your square unique to you (habit tracking anyone?)</p>
+            </div>
+        </div>
+        <form>
+            <div class="form-group row">
+                <label for="email" class="col-sm-4 col-form-label">custom input 1</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" id="test-input" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-4 col-form-label">custom input 2</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-4 col-form-label">custom input 3</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-4 col-form-label">custom input 4</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-4 col-form-label">custom input 5</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+        </form>
                 
             </LocalForm>
         );
